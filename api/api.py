023c2995d389cfe208fea956c8685576464017f1
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-08-21 17:32:33 -0400

    Initial: Flask and React with formated time at /time endpoint

---
+++ api/api.py
@@ -0,0 +1,14 @@
+import time
+from flask import Flask
+
+app = Flask(__name__)
+
+# Will return a JSON payload like: {"time": 1589144576.033372}
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
+
+
+@app.route('/whoisthegreatest')
+def get_greatest():
+    return {'greatest': 'Ann Cascarano'}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    const fetchCurrentTime = async () => {
+      const response = await fetch("/time");
+      const data = await response.json();
+      setCurrentTime(data.time);
+    };
+    fetchCurrentTime();
+  }, []);
+
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    timeZoneName: "short"
+  };
+  const formatTime = new Date(currentTime * 1000).toLocaleString("en-US", options);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header>
+        <h1>Ann experiments with Flask and React! Woo!</h1>
       </header>
+      {formatTime}
     </div>
   );
 }
