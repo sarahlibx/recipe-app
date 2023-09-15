5bf133378d0758975d3033980bc7e0ea908d85bb
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-09-15 15:26:33 -0400

    A bit more structure and styling around the search button

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,13 @@ const Header = ({ showRecipeForm, updateSearchTerm, searchTerm }) => {
   return (
     <header>
       <h1>My Favorite Recipes</h1>
-      <button onClick={showRecipeForm}>Add New Recipe</button>
-      <input type='text' value={searchTerm} onChange={(e) => updateSearchTerm(e.target.value)} />
+      <div class='header-actions'>
+        <button onClick={showRecipeForm}>Add New Recipe</button>
+        <div className='search'>
+          <label htmlFor='search'>Search</label>
+          <input type='text' id='search' value={searchTerm} onChange={(e) => updateSearchTerm(e.target.value)} />
+        </div>
+      </div>
     </header>
   );
 };
