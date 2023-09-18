34593ba3cf3f901a1160cac6eb67df098b1e9df4
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-09-18 10:51:56 -0400

    Add react-toastify and try/catch blocks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import RecipeFull from "./components/RecipeFull";
 import NewRecipeForm from "./components/NewRecipeForm";
 import RecipeExcerpt from "./components/RecipeExcerpt";
@@ -40,40 +43,6 @@ function App() {
     return searchResults;
   };
 
-  const updateSearchTerm = (text) => {
-    setSearchTerm(text);
-  };
-  const handleUpdateRecipe = async (e, selectedRecipe) => {
-    e.preventDefault();
-    const { id, title, ingredients, instructions, servings, image_url, description } = selectedRecipe;
-    const response = await fetch(`/api/recipes/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ title, ingredients, instructions, servings, description, image_url })
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      console.log({ data });
-      // Update the recipe in the frontend
-      setRecipes(
-        recipes.map((recipe) => {
-          if (recipe.id === id) {
-            // Return the saved data from the db
-            return data.recipe;
-          }
-          return recipe;
-        })
-      );
-    } else {
-      console.error("Recipe update failed.");
-    }
-
-    setSelectedRecipe(null);
-  };
-
   const onUpdateForm = (e, action = "new") => {
     const { name, value } = e.target;
     if (action === "update") {
@@ -87,38 +56,110 @@ function App() {
     }
   };
 
+  const updateSearchTerm = (text) => {
+    setSearchTerm(text);
+  };
+
   const handleNewRecipe = async (e, newRecipe) => {
     e.preventDefault();
-    console.log("Adding recipe");
 
     const { title, ingredients, instructions, servings, description, image_url } = newRecipe;
-    const response = await fetch("/api/recipes", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ title, ingredients, instructions, servings, description, image_url })
-    });
 
-    if (response.ok) {
-      const data = await response.json();
-
-      console.log({ data });
-      // Update the recipe in the frontend
-      setRecipes([...recipes, data.recipe]);
-
-      setShowNewRecipeForm(false);
-      setNewRecipe({
-        title: "",
-        ingredients: "",
-        instructions: "",
-        description: "",
-        imageUrl: "",
-        servings: 1
+    try {
+      const response = await fetch("/api/recipes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ title, ingredients, instructions, servings, description, image_url })
       });
-    } else {
-      console.error("Recipe update failed.");
+
+      if (response.ok) {
+        const data = await response.json();
+
+        console.log({ data });
+        // Update the recipe in the frontend
+        setRecipes([...recipes, data.recipe]);
+
+        toast.success("Recipe added successfully!", {
+          position: "bottom-right",
+          autoClose: 3000
+        });
+
+        setShowNewRecipeForm(false);
+        setNewRecipe({
+          title: "",
+          ingredients: "",
+          instructions: "",
+          description: "",
+          imageUrl: "",
+          servings: 1
+        });
+      } else {
+        toast.error("Oops - could not add recipe!", {
+          position: "bottom-right",
+          autoClose: 3000
+        });
+      }
+    } catch (e) {
+      console.e("An error occurred during the request:", e);
+
+      toast.error("An unexpected error occurred. Please try again later.", {
+        position: "bottom-right",
+        autoClose: 3000
+      });
     }
+  };
+
+  const handleUpdateRecipe = async (e, selectedRecipe) => {
+    e.preventDefault();
+    const { id, title, ingredients, instructions, servings, image_url, description } = selectedRecipe;
+
+    try {
+      const response = await fetch(`/api/recipes/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ title, ingredients, instructions, servings, description, image_url })
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log({ data });
+
+        setRecipes(
+          recipes.map((recipe) => {
+            if (recipe.id === id) {
+              // Return the saved data from the db
+              return data.recipe;
+            }
+            return recipe;
+          })
+        );
+
+        toast.success("Recipe updated!", {
+          position: "bottom-right",
+          autoClose: 3000
+        });
+      } else {
+        console.error("Recipe update failed.");
+
+        toast.error("Failed to update recipe. Please try again.", {
+          position: "bottom-right",
+          autoClose: 3000
+        });
+      }
+    } catch (error) {
+      console.error("An error occurred during the request:", error);
+
+      toast.error("An unexpected error occurred. Please try again later.", {
+        position: "bottom-right",
+        autoClose: 3000
+      });
+    }
+
+    setSelectedRecipe(null);
   };
 
   const handleDeleteRecipe = async (recipeId) => {
@@ -130,11 +171,22 @@ function App() {
       if (response.ok) {
         setRecipes(recipes.filter((recipe) => recipe.id !== recipeId));
         setSelectedRecipe(null);
+        toast.success("Recipe deleted successfully!", {
+          position: "bottom-right",
+          autoClose: 3000
+        });
       } else {
-        console.error("Welp - could not delete recipe.");
+        toast.error("Oops - could not delete recipe!", {
+          position: "bottom-right",
+          autoClose: 3000
+        });
       }
     } catch (e) {
-      console.error("Something went wrong:", e);
+      console.error("Something went wrong during the request:", e);
+      toast.error("An unexpected error occurred. Please try again later.", {
+        position: "bottom-right",
+        autoClose: 3000
+      });
     }
   };
 
@@ -190,6 +242,7 @@ function App() {
           ))}
         </div>
       )}
+      <ToastContainer />
     </div>
   );
 }
